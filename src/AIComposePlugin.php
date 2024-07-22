9ebc9b9f56a8ed7dfb0d8ba4ad3f19b0bea9e5c0
harun_d <harun.duranovic@dooherceg.ba>
2024-07-22 13:16:28 +0200

    Dodat composer.json fajl, uredjena struktura projekta te dodat cs fixer za rad prema standardima rcube-a

---
+++ src/AIComposePlugin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace HercegDoo\AIComposePlugin;
+
+class AIComposePlugin extends rcube_plugin
+{
+    public $task = 'mail';
+
+    public function init()
+    {
+        $this->add_hook('render_page', [$this, 'load_resources']);
+    }
+
+    public function load_resources($args)
+    {
+        if ($args['template'] == 'compose' || $args['template'] == 'settings') {
+            $this->include_stylesheet('./assets/styles/styles.css');
+            $this->include_script('./assets/scripts/main.js');
+        }
+
+        return $args;
+    }
+}
