38228106952a71b48f51f34c544b8c46c2963abb
harun_d <harun.duranovic@dooherceg.ba>
2024-07-22 14:39:22 +0200

    Dodana GitHub Akcija za provjeru stila koda (Provjera sa ispravno formatiranim kod stilom)

--- src/AIComposePlugin.php
+++ src/AIComposePlugin.php
@@ -13,36 +13,14 @@ class AIComposePlugin extends rcube_plugin
 
     public function load_resources($args)
     {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         $this->include_stylesheet('./css/settings.css');
 
-        if ($args['template'] == 'compose' ) {
+        if ($args['template'] == 'compose') {
             $this->include_stylesheet('./assets/styles/composeStyles.css');
             $this->include_script('./assets/scripts/composeMain.js');
-
-        }elseif ($args['template'] == 'settings') {
+        } elseif ($args['template'] == 'settings') {
             $this->include_stylesheet('./assets/styles/settingsStyles.css');
             $this->include_script('./assets/scripts/settingsMain.js');
-
         }
 
         return $args;
