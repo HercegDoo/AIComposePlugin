68a250ef34e0806bc58a842da4d98aa562f4944b
harun_d <harun.duranovic@dooherceg.ba>
2024-07-23 12:46:06 +0200

    Dodat kod za dugme u compose sekciji

--- assets/scripts/composeMain.js
+++ assets/scripts/composeMain.js
@@ -1,5 +1,28 @@
-//file za inicijalizaciju dugmeta i modala
+document.addEventListener("DOMContentLoaded", function () {
+    // Kreiranje <li> elementa
+    console.log(44);
 
-let liElement = document.createElement('li');
-let aElement = document.createElement('a');
-let spanElement = document.createElement('span');
+    let liElement = document.createElement("li");
+    liElement.setAttribute("role", "menuitem");
+
+    // Kreiranje <a> elementa
+    var aElement = document.createElement("a");
+    aElement.setAttribute("href", "#responses");
+    aElement.setAttribute("id", "aicp-prompt-open-button");
+    aElement.classList.add("auto-generate");
+
+    // Kreiramo span element i dodajemo tekst
+    var spanElement = document.createElement("span");
+    spanElement.classList.add("inner");
+    spanElement.innerText = "Auto-Generate";
+
+    // Dodajemo span u a element
+    aElement.appendChild(spanElement);
+
+    // Dodajemo a element u <li> element
+    liElement.appendChild(aElement);
+
+    // Dodajemo <li> element u toolbar-menu
+    let parentEl = document.querySelector("#toolbar-menu");
+    parentEl.appendChild(liElement);
+});
