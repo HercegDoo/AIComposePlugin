90334ba6d01fca7f5f9692397c274cf4bc649ded
harun_d <harun.duranovic@dooherceg.ba>
2024-07-27 12:30:43 +0200

    testiranje hooka

--- src/AbstractAIComposePlugin.php
+++ src/AbstractAIComposePlugin.php
@@ -9,7 +9,7 @@ use rcube_plugin;
 abstract class AbstractAIComposePlugin extends rcube_plugin
 {
     public $task = 'mail|settings';
-//test komentarr
+//test komentarrr
     public function init(): void
     {
         $task = $this->api->task;
