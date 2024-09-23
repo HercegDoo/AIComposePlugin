a1000957e2a190e3c722f07b72ae58a5aede26f0
harun_d <harun.duranovic@dooherceg.ba>
2024-09-23 03:32:50 -1000

    dodat izgled na settings stranici od ocekivanih dropdowna, end day commit

--- assets/src/settings.js
+++ assets/src/settings.js
@@ -1,1 +1,8 @@
 import "./settings/style.css";
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  rcmail.addEventListener('settings-save', ()=>{
+    console.log('nres');
+  })
+
+})
--- src/Tasks/SettingsTask.php
+++ src/Tasks/SettingsTask.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace HercegDoo\AIComposePlugin\Tasks;
 
+use HercegDoo\AIComposePlugin\AIEmailService\Settings;
+
 class SettingsTask extends AbstractTask
 {
     public function init(): void
     {
         $this->plugin->add_hook('preferences_sections_list', [$this, 'preferencesSectionsList']);
         $this->plugin->add_hook('preferences_list', [$this, 'preferencesList']);
+        $this->plugin->add_hook('preferences_save', [$this, 'preferencesSave']);
     }
 
     /**
@@ -19,6 +22,8 @@ class SettingsTask extends AbstractTask
      */
     public function preferencesSectionsList(array $args): array
     {
+        error_log("EVO ERRORAAAA");
+        error_log(print_r($args, true));
         /** @var array<string, array<string, mixed>> $list */
         $list = $args['list'] ?? [];
 
@@ -48,13 +53,21 @@ class SettingsTask extends AbstractTask
                 'name' => 'General Settings',
                 'options' => [
                     [
-                        'title' => 'Option 1',
-                        'content' => 'Content for Option 1',
+                        'title' => 'Style',
+                        'content' => $this->getDropdownHtml(Settings::getStyles(), 'style'),
                     ],
                     [
-                        'title' => 'Option 2',
-                        'content' => 'Content for Option 2',
+                        'title' => 'Creativity',
+                        'content' => $this->getDropdownHtml(Settings::getCreativities(), 'creativity'),
                     ],
+                    [
+                        'title' => 'Length',
+                        'content' => $this->getDropdownHtml(Settings::getLengths(), 'length'),
+                    ],
+                    [
+                        'title' => 'Language',
+                        'content' => $this->getDropdownHtml(Settings::getLanguages(), 'language'),
+                    ]
                 ],
             ];
 
@@ -63,4 +76,98 @@ class SettingsTask extends AbstractTask
 
         return $args;
     }
+
+
+
+    private function getDropdownHtml(array $options, string $name): string
+    {
+        $dropdown = '<select name="data[aic][' . $name . ']">'; // Ispravno ime za formu
+        foreach ($options as $option) {
+            $dropdown .= '<option value="' . htmlspecialchars($option) . '">' . htmlspecialchars($option) . '</option>';
+        }
+        $dropdown .= '</select>';
+        return $dropdown;
+    }
+
+
+    /**
+     * @param array<string, mixed> $args
+     *
+     * @return array<string, mixed>
+     */
+    /**
+     * @param array<string, mixed> $args
+     *
+     * @return array<string, mixed>
+     */
+    /**
+     * @param array<string, mixed> $args
+     *
+     * @return array<string, mixed>
+     */
+    /**
+     * @param array<string, mixed> $args
+     *
+     * @return array<string, mixed>
+     */
+    public function preferencesSave(array $args): array
+    {
+        error_log("POCETNI");
+        error_log(print_r($args, true)); // Prikazivanje sadržaja args
+
+        // Inicijalizuj prazan niz za sačuvane opcije
+        $savedOptions = [];
+
+        // Proveri da li 'data' postoji i da je niz
+        if (isset($args['data']) && is_array($args['data'])) {
+
+            error_log("JOS VANJSKI");
+
+            // Proveri da li su podaci vezani za odeljak 'aic'
+            if (isset($args['data']['aic']) && is_array($args['data']['aic'])) {
+                $aicData = $args['data']['aic'];
+                         error_log("VANJSKI");
+                // Sačuvaj stil
+                if (isset($aicData['style'])) {
+                    $savedOptions['style'] = $aicData['style'];
+                    error_log("MOJ SACUVANI STIL");
+                    error_log(print_r($aicData['style'], true));
+                }
+
+                // Sačuvaj kreativnost
+                if (isset($aicData['creativity'])) {
+                    $savedOptions['creativity'] = $aicData['creativity'];
+                }
+
+                // Sačuvaj dužinu
+                if (isset($aicData['length'])) {
+                    $savedOptions['length'] = $aicData['length'];
+                }
+
+                // Sačuvaj jezik
+                if (isset($aicData['language'])) {
+                    $savedOptions['language'] = $aicData['language'];
+                }
+                error_log("OVDJE SAVED OPTIONS - NOVI");
+                error_log(print_r($savedOptions, true));
+
+                // Ovde možeš dalje obrađivati $savedOptions, kao što je čuvanje u bazi podataka
+                error_log('Saved options: ' . print_r($savedOptions, true));
+            } else {
+                error_log('No valid data found in args[data][aic]');
+            }
+        } else {
+            error_log('args[data] is not set or not an array.');
+        }
+
+
+        return $args;
+    }
+
+
+
+
+
+
+
 }
