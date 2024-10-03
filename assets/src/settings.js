613e01902b2d4950f4e69cf3b498e097eeb024c8
harun_d <harun.duranovic@dooherceg.ba>
2024-10-03 01:41:41 -1000

    Uklonjena greska prilikom prikazivanja na klik create

--- assets/src/settings.js
+++ assets/src/settings.js
@@ -1,6 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', ()=>{
-  rcmail.enable_command('add', true);
+  // rcmail.enable_command('add', true);
   const createButton = document.querySelector('a.create');
   createButton.addEventListener('click', ()=>{
   rcmail.location_href({_action: "plugin.AIComposePlugin_AddInstruction", _id: 0 , _framed: 1}, rcmail.get_frame_window(rcmail.env.contentframe), true)
