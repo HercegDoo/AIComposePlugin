d4e3cbe2fb50650429f295fcaca43f630d072a97
harun_d <harun.duranovic@dooherceg.ba>
2024-12-04 03:56:04 -1000

    Dodat path na cookie u js

--- assets/src/compose/emailHelpers/instructionHeightHandler.js
+++ assets/src/compose/emailHelpers/instructionHeightHandler.js
@@ -5,7 +5,7 @@ export function handleInstructionHeight(){
 
     const diff = textarea.getBoundingClientRect().height - entries[0].contentRect.height;
     if(diff >= 0){
-    document.cookie = `textareaxHeight=${entries[0].contentRect.height + diff}; expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/`;
+    document.cookie = `textareaxHeight=${entries[0].contentRect.height + diff}; expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/mail`;
     }
   });
   observer.observe(textarea)
