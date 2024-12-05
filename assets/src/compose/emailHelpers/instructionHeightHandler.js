7e2deef80837381d7c849bbb5fd00c544de344c8
harun_d <harun.duranovic@dooherceg.ba>
2024-12-04 20:35:31 -1000

    Blago izmjenjena logika pamcenja visine textarea, ispravljena sintaksicka greska u css

--- assets/src/compose/emailHelpers/instructionHeightHandler.js
+++ assets/src/compose/emailHelpers/instructionHeightHandler.js
@@ -1,12 +1,11 @@
 export function handleInstructionHeight(){
   const textarea = document.getElementById('aic-instruction');
 
-  const observer = new ResizeObserver((entries)=>{
+  const observer = new ResizeObserver(()=>{
 
-    const diff = textarea.getBoundingClientRect().height - entries[0].contentRect.height;
-    if(diff >= 0){
-    document.cookie = `textareaxHeight=${entries[0].contentRect.height + diff}; expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/mail`;
-    }
+
+    document.cookie = `textareaxHeight=${textarea.getBoundingClientRect().height }; expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/mail`;
+
   });
   observer.observe(textarea)
 
