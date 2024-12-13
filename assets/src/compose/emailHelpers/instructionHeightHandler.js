0aa34a1342acf924773a0edb648548f09669c5d1
Harun2244 <153545164+Harun2244@users.noreply.github.com>
2024-12-13 02:00:56 -1000

    Implemented the height increase of the instructions-textarea when text enters into the next row (#114)
    * Implemented the increase of the instructions-textarea when text enters the next row
    
    * Removed unnecessary console log

--- assets/src/compose/emailHelpers/instructionHeightHandler.js
+++ assets/src/compose/emailHelpers/instructionHeightHandler.js
@@ -20,3 +20,50 @@ export function handleInstructionHeight(){
 
 
 }
+
+export function expandInstructionHeightBasedOnInput(){
+  const textarea = document.getElementById('aic-instruction');
+  textarea.style.overflow = "hidden";
+
+  const hiddenDiv = document.createElement('div');
+  hiddenDiv.style.position = 'absolute';
+  hiddenDiv.style.visibility = 'hidden';
+  hiddenDiv.style.whiteSpace = 'pre-wrap';
+  hiddenDiv.style.overflowWrap = 'break-word';
+  document.body.appendChild(hiddenDiv);
+
+
+  const textareaLineHeight = parseFloat(window.getComputedStyle(textarea).lineHeight);
+
+  textarea.addEventListener('input', (event) => {
+    syncStyles(textarea, hiddenDiv);
+    const textareaHeight = textarea.getBoundingClientRect().height;
+
+    const value = textarea.value;
+    const cursorPosition = textarea.selectionStart;
+
+    hiddenDiv.textContent = value.substring(0, cursorPosition);
+
+    const totalLines = Math.floor(textarea.scrollHeight / textareaLineHeight);
+    if(totalLines === 2 && textareaShouldExpand(textareaLineHeight,textareaHeight)){
+      textarea.style.height = textareaHeight + textareaLineHeight  + `px`;
+      textarea.style.overflow = "auto";
+    }
+    else if(totalLines === 1){
+      textarea.style.overflow = "hidden";
+    }
+  });
+}
+
+function textareaShouldExpand(lineHeight, textareaHeight){
+  return textareaHeight <= lineHeight * 2;
+}
+
+function syncStyles(source, target) {
+  const computedStyles = window.getComputedStyle(source);
+  target.style.font = computedStyles.font;
+  target.style.padding = computedStyles.padding;
+  target.style.border = computedStyles.border;
+  target.style.width = `${source.offsetWidth}px`;
+  target.style.lineHeight = computedStyles.lineHeight;
+}
